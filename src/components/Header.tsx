6c0511877052e24291151369751198e7db95c1ce
ValenButtignol <valentinbuttignol@gmail.com>
2025-10-08 18:38:30 -0300

    Add header but lacking of alignment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,18 @@
+import logo from '../assets/logo.svg';
+import '../styles/Header.css';
+//className="fixed top-0 left-0 w-full px-8 py-4 flex justify-between items-center backdrop-blur-sm bg-black/20 z-20"
+
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 w-full px-8 py-4 flex justify-between items-center backdrop-blur-sm bg-black/20 z-20">
-      
-      <nav className="flex gap-6 text-sm">
-        <a href="#about" className="hover:text-cyan-300">About</a>
-        <a href="#experience" className="hover:text-cyan-300">Experience</a>
-        <a href="#projects" className="hover:text-cyan-300">Projects</a>
-        <a href="#contact" className="hover:text-cyan-300">Contact</a>
+    <header className="header">
+      <a href="/" className='header-logo'><img src={logo}/></a>
+      <nav className="header-nav">
+        <a href="/" className="header-link">Home</a>
+        <a href="#about" className="header-link">About</a>
+        <a href="#experience" className="header-link">Experience</a>
+        <a href="#projects" className="header-link">Projects</a>
+        <a href="#contact" className="header-link">Contact</a>
+        <a href="https://drive.google.com/file/d/17ptu4h42nAPc2q-FDQd3RdsSoQesmJZY/view?usp=sharing" target="_blank" className="header-resume">Resume</a>
       </nav>
     </header>
   )
