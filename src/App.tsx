35546fc635593d1145a6f4ebbb0540c7b9fb618f
ValenButtignol <valentinbuttignol@gmail.com>
2025-10-09 19:08:28 -0300

    Add new epic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './styles/layout.css'
 import Introduction from './components/Introduction';
 import About from './components/About';
 import Experience from './components/Experience';
@@ -7,19 +7,25 @@ import Contact from './components/Contact';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HexBackground from './components/HexBackground';
+import LeftSidebar from './components/LeftSideBar';
+import RightSidebar from './components/RightSideBar';
 
 function App() {
   return (
-    <div className="relative min-h-screen text-white">
+    <div className="app-root">
       <HexBackground />
-      <main className="relative z-10">
-        <Header/>
-        <Introduction/>
-        <About/>
-        <Experience/>
-        <Projects/>
-        <Contact/>
-        <Footer/>
+      <Header/>
+      <LeftSidebar />
+      <RightSidebar />
+      <main className="content-wrapper">
+        <div className="content">
+          <Introduction/>
+          <About/>
+          <Experience/>
+          <Projects/>
+          <Contact/>
+          <Footer/>
+        </div>
       </main>
     </div>
   )
