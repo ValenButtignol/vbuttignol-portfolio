0d7198eec41291e3f62d9d8501eb2803de9981db
ValenButtignol <valentinbuttignol@gmail.com>
2025-10-09 18:03:56 -0300

    Add Comfortaa to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import logo from '../assets/logo.svg';
-import '../styles/Header.css';
+import '../styles/header.css';
 
 const Header = () => {
   const [hide, setHide] = useState(false);
@@ -23,11 +23,11 @@ const Header = () => {
     <header className={`header${hide ? " header--hidden" : ""}`}>
       <a href="/" className='header-logo'><img src={logo}/></a>
       <nav className="header-nav">
-        <a href="/" className="header-link">Home</a>
-        <a href="#about" className="header-link">About</a>
-        <a href="#experience" className="header-link">Experience</a>
-        <a href="#projects" className="header-link">Projects</a>
-        <a href="#contact" className="header-link">Contact</a>
+        <a href="/" className="header-link"><span className='header-bullet'>•</span> Home</a>
+        <a href="#about" className="header-link"><span className='header-bullet'>•</span> About</a>
+        <a href="#experience" className="header-link"><span className='header-bullet'>•</span> Experience</a>
+        <a href="#projects" className="header-link"><span className='header-bullet'>•</span> Projects</a>
+        <a href="#contact" className="header-link"><span className='header-bullet'>•</span> Contact</a>
         <a href="https://drive.google.com/file/d/17ptu4h42nAPc2q-FDQd3RdsSoQesmJZY/view?usp=sharing" target="_blank" className="header-resume">Resume</a>
       </nav>
     </header>
