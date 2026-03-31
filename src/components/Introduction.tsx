494d85a7b2c96efb9efb3f218ffc043ab992b3bf
ValenButtignol <valentinbuttignol@gmail.com>
2026-03-30 23:46:02 -0300

    Keep adding changes to about and intro sections

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -9,13 +9,9 @@ const Introduction = () => {
         <div className="intro-text">
           <p className="gray-font elegant-font">Hi there! My name is</p>
           <h1 className='lb-font'>Valentin Buttignol</h1>
-          <h2 className="darkpurple-font">and I'm a Software Engineer.</h2>
+          <h2 className="darkpurple-font">Software Engineer at <span className='smoothpurple-font bold'>ExxonMobil</span>.</h2>
           <p className='gray-font'>
-            I specialize in backend development, with a professional experience and 
-            strong academic foundation in building reliable and scalable software. 
-            Passionate about creating clean, efficient solutions and continuously 
-            learning new technologies.
-          </p>
+            I design and build clean, reliable full-stack applications, focusing on performance, scalability, and a smooth user experience.          </p>
         </div>
         <HexStack />
       </div>
